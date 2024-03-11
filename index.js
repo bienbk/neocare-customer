e575944ea5155cbcc42e1bd86650896f6f0c1748
phucqn <quachphucbk@gmail.com>
2024-03-12 00:18:25 +0700

    fix api by env

--- index.js
+++ index.js
@@ -5,10 +5,15 @@
 import {AppRegistry} from 'react-native';
 import {UrlApi} from 'http/UrlApi';
 import SuperTokens from 'supertokens-react-native';
+import {
+  BASE_PATH_CAFE,
+  BASE_NEOCARE_AUTH,
+  BASE_NEOCARE_CUSTOMER,
+} from 'assets/config';
 import App from './src/app';
 import {name as appName} from './app.json';
 SuperTokens.init({
-  apiDomain: 'https://dev-api.neocare.vn',
+  apiDomain: BASE_NEOCARE_AUTH,
   apiBasePath: '/patients/auth',
 });
 console.log('SuperTokens index: ', SuperTokens);
